ef3bb30f8e1bcc6a40b5e310991ed11297cb24f8
Karina Lorenzo <karinalrg16@gmail.com>
2025-03-25 17:01:18 +0100

    feat-class Truck implemented.Test passed

---
+++ src/main/java/dev/karina/Truck.java
@@ -0,0 +1,15 @@
+package dev.karina;
+
+public class Truck extends Vehicle {
+    private final int numAxles;
+
+    public Truck(String plate, int numAxles) {
+        super(plate);
+        this.numAxles = numAxles;
+    }
+
+    @Override
+    public int calculateToll(int i) {
+        return numAxles * 50;
+    }
+}
