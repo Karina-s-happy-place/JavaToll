a697ac50873ba8edb8773bcc75d6f53826e669dc
Karina Lorenzo <karinalrg16@gmail.com>
2025-03-25 17:51:11 +0100

    feat-testTruckWithZeroAxles implemented.Test failed

--- src/test/java/dev/karina/TruckTest.java
+++ src/test/java/dev/karina/TruckTest.java
@@ -1,6 +1,7 @@
 package dev.karina;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,5 +10,13 @@ public class TruckTest {
     public void testTruckTollCalcultaion() {
         Vehicle truck = new Truck("GHI789", 4);
         assertEquals(200, truck.calculateToll(0));
+    }
+
+    @Test
+    public void testTruckWithZeroAxles() {
+        assertThrows(IllegalStateException.class, () -> {
+            Vehicle truck = new Truck("JKL012", 0);
+            truck.calculateToll(0);
+        });
     }
 }
