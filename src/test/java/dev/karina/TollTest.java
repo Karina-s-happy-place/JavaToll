cc970040b81837d128265f2032e246d762e65e28
Karina Lorenzo <karinalrg16@gmail.com>
2025-03-26 23:47:14 +0100

    feat- testGetRegisteredVehicles implemented. Test failed

--- src/test/java/dev/karina/TollTest.java
+++ src/test/java/dev/karina/TollTest.java
@@ -2,6 +2,8 @@ package dev.karina;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class TollTest {
@@ -14,5 +16,21 @@ public class TollTest {
         toll.registerVehicle(car);
 
         assertEquals(100, toll.getTotalAmount());
+    }
+
+    @Test
+    public void testGetRegisteredVehicles() {
+        Toll toll = new Toll("North Toll", "Oviedo");
+        Vehicle car = new Car("ABC123");
+        Vehicle car2 = new Car("XYZ456");
+
+        toll.registerVehicle(car);
+        toll.registerVehicle(car2);
+
+        List<Vehicle> vehicles = toll.getVehicles();
+
+        assertEquals(2, vehicles.size());
+        assertEquals("ABC123", vehicles.get(0).getPlate());
+        assertEquals("XYZ456", vehicles.get(1).getPlate());
     }
 }
