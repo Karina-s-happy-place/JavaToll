983dc55f1d4b0793abb75ebeb0c544152cca39e4
Karina Lorenzo <karinalrg16@gmail.com>
2025-03-26 23:14:45 +0100

    feat-testRegisterSingleVehicle implemented. Test failed

---
+++ src/test/java/dev/karina/TollTest.java
@@ -0,0 +1,18 @@
+package dev.karina;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TollTest {
+
+    @Test
+    public void testRegisterSingleVehicle() {
+        Toll toll = new Toll("North Toll", "Oviedo");
+        Vehicle car = new Car("ABC123");
+
+        toll.registerVehicle(car);
+
+        assertEquals(toll, toll.getTotalAmount());
+    }
+}
